cb8021902a9a9b325e0cf3db64abbe3c02944a9d
Dan Nolan <dan.james.nolan@gmail.com>
2012-09-11 17:54:18 +1000

    Fixing some elements

--- models/init.rb
+++ models/init.rb
@@ -2,8 +2,10 @@ APP_ROOT = File.expand_path(File.dirname(__FILE__)) unless defined?(APP_ROOT)
 
 DataMapper.setup(:default, "sqlite:///#{APP_ROOT}/custinfo.db")
 
-DataMapper.auto_upgrade!
 load 'models/checkin.rb'
 load 'models/session.rb'
 load 'models/user.rb'
 load 'models/venue.rb'
+
+
+DataMapper.auto_upgrade!
