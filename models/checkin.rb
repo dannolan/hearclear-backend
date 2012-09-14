a7f7ad750f175610e2ff3779be38b3c1bf254512
Dan Nolan <dan.james.nolan@gmail.com>
2012-09-14 15:06:10 +1000

    Adding some modifications and cleaning up some other code

--- models/checkin.rb
+++ models/checkin.rb
@@ -13,6 +13,15 @@ class Checkin
 	#has_many sessions
 	
 	
+	def self.user_count_for_venue(user,venue)
+		Checkin.all(:user => user, :venue => venue).count
+	end
+	
+	
+	def self.sessions_count_for_user(user)
+		Checkin.all(:user_id => venue).sessions.count
+	end
+	
 	#start_time
 	
 	#end_time
