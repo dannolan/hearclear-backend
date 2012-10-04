cf0a68894cf5650b0aa0f0102882fe5530c37094
Dan Nolan <dan.james.nolan@gmail.com>
2012-10-04 11:44:18 +1000

    Putting in other values for information

--- models/checkin.rb
+++ models/checkin.rb
@@ -12,6 +12,31 @@ class Checkin
 	#belongs_to venue
 	#has_many sessions
 	
+	def process_outliers
+		levels = self.sessions.all.collect(&:averageLevel)
+		std_dev = levels.standard_deviation
+		mean = levels.mean
+		q1 = mean - std_dev
+		q2 = mean + std_dev
+		self.sessions.each do |session|
+			avgLevel = session.averageLevel
+			if avgLevel >= q1 && avgLevel <= q2
+				session.outlier = false
+			else
+				session.outlier = true
+			end
+			session.save
+		end
+	end
+	
+	
+	def checkin_avg
+		self.sessions.avg(:averageLevel, :outlier => false)
+	end
+	
+	def checkin_peak
+		self.sessions.avg(:maxLevel, :outlier => false)
+	end
 	
 	def self.user_count_for_venue(user,venue)
 		Checkin.all(:user => user, :venue => venue).count
